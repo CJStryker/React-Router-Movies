21c387df7a8fb8fa4c31ec9ddce314fe64334adc
Christopher Jospeh Stryker <chrisjstryker@gmail.com>
2020-01-15 12:28:26 -0500

    MVP Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import { Route } from 'react-router-dom';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -11,8 +14,9 @@ const App = () => {
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+          <SavedList exact path='/' list={savedList} />
+          <Route exact path='/' component={MovieList} />
+          <Route path='/movies/:id' component={Movie} />
     </div>
   );
 };
